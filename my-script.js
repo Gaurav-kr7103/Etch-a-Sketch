10983359e1c8a0182eec80b9fe2f9c14cf327e94
Gaurav Kumar <gauravkr7103@gmail.com>
2025-03-27 00:29:46 +0530

    complete the project mentioned in the TOP
    etch the sketch is complete the grids will now be
    created with user number input

--- my-script.js
+++ my-script.js
@@ -1,17 +1,36 @@
 const container = document.querySelector(".container");
 
-for (let i=0; i<16; i++) {
-    for (let j=0; j<16; j++) {
-        const div = document.createElement("div");
-        div.classList.add("box");
-        container.appendChild(div);
+createGird(16);
+
+function createGird (gridNumber) {
+
+    container.innerHTML = "";//to clear the container(resetting the grid)
+
+    let size = gridNumber;
+    for (let i=0; i<size; i++) {
+        for (let j=0; j<size; j++) {
+            const div = document.createElement("div");
+            div.classList.add("box");
+            div.style.width = (100 / size) + "%";
+            div.style.height = (100 / size) + "%";
+            container.appendChild(div);
+        }
     }
+    
+    const boxes = document.querySelectorAll(".box");
+    // console.log (boxes.length);
+    boxes.forEach(box => {
+        box.addEventListener("mouseover", () => {
+            box.style.backgroundColor = "black";
+        });
+    });
 }
 
-const boxes = document.querySelectorAll(".box");
-// console.log (boxes.length);
-boxes.forEach(box => {
-    box.addEventListener("mouseover", () => {
-        box.style.backgroundColor = "black";
-    });
-});
+const resetButton = document.querySelector("#reset");
+resetButton.addEventListener("click", () => {
+    let gridNumber = 0;
+    do {
+        gridNumber = prompt("Enter the number of squares per side");
+    } while (gridNumber < 1 || gridNumber > 100);
+    createGird(gridNumber);
+})
